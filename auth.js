bab605adee2168d016cc779834381de4d247bebd
AyKay4 <nguyenthuannha44@gmail.com>
2026-09-16 13:30:00 +0700

    Create auth.js

---
+++ auth.js
@@ -0,0 +1,9 @@
+function login(username, password) {
+  if (username === 'admin' && password === '123') {
+    return true;
+  }
+  return false;
+}
+if (typeof module !== 'undefined' && module.exports) {
+  module.exports = { login };
+}
